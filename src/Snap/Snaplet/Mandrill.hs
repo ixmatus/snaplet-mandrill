02d7922c14454a1d7a9a8d0e4eb9c0cc50fc1cf6
Parnell Springmeyer <parnell@ixmat.us>
2014-09-17 16:46:26 -0500

    Minor fixes

--- src/Snap/Snaplet/Mandrill.hs
+++ src/Snap/Snaplet/Mandrill.hs
@@ -1,46 +1,40 @@
-{-# LANGUAGE ConstraintKinds     #-}
-{-# LANGUAGE FlexibleContexts    #-}
-{-# LANGUAGE FlexibleInstances   #-}
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 
 module Snap.Snaplet.Mandrill
   ( initMandrill
   , runMandrill
-  , mkAmqpConn
-  , Mandrill    (..)
-  , HasMandrill (..)
+  , MandrillState (..)
+  , HasMandrill   (..)
   ) where
 
+import           Control.Applicative
 import           Control.Monad.State
 import           Control.Monad.Trans.Reader
 import           Data.Configurator
-import           Data.Configurator.Types
-import           Network.API.Mandrill
+import           Network.API.Mandrill       (MandrillKey)
 import           Paths_snaplet_mandrill
 import           Snap.Snaplet
 
 -------------------------------------------------------------------------------
-newtype Mandrill = Mandrill { token :: MandrillKey }
+newtype MandrillState = MandrillState { token :: MandrillKey }
 
 -------------------------------------------------------------------------------
 class MonadIO m => HasMandrill m where
     getMandrill :: m MandrillKey
 
-instance HasAmqpConn (Handler b Mandrill) where
-    getMandrill = get
+instance HasMandrill (Handler b MandrillState) where
+    getMandrill = gets token
 
 instance MonadIO m => HasMandrill (ReaderT MandrillKey m) where
     getMandrill = ask
 
 -- | Initialize the Mandrill Snaplet.
-initMandrill :: SnapletInit b Mandrill
+initMandrill :: SnapletInit b MandrillState
 initMandrill = makeSnaplet "mandrill" description datadir $ do
-    conf  <- getSnapletUserConfig
-    token <- liftIO $ require conf "token"
-
-    return $ Mandrill token
+    conf <- getSnapletUserConfig
+    return =<< MandrillState <$> (liftIO $ require conf "token")
 
   where
     description = "Snaplet for Mandrill library"
@@ -48,5 +42,5 @@ initMandrill = makeSnaplet "mandrill" description datadir $ do
 
 -------------------------------------------------------------------------------
 -- | Runs an Mandrill action in any monad with a HasAmqpConn instance.
--- runAmqp :: (HasAmqpConn m) => (AmqpC -> b) -> m b
--- runAmqp action = getAmqpConn >>= return . action
+runMandrill :: (HasMandrill m) => (MandrillKey -> b) -> m b
+runMandrill action = getMandrill >>= return . action
